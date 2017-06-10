bda12ed429d62bb3d4c31a8683bf944b6513bd0c
Martin <martin@martin.com>
2017-06-10 16:38:43 +0100

    wip different article types

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,40 +1,30 @@
 import React from 'react';
-import Interactive from 'react-interactive';
-import { Switch, Route, Link } from 'react-router-dom';
 
-function ArticleLink (props) {
-    if (props.articleType === 'click-here-to-read') {
-        return (
-            <Link to={`/articles/${props.articleName}`}>
-                Click here to read
-            </Link>
-        );
-    } else if (props.articleType === 'js-accordian') {
-        return (
-            <div className="accordian-click-to-expand">+ Click to expand</div>
-        );
+import AccordianArticle from './DefaultArticle';
+import DefaultArticle from './DefaultArticle';
+
+function returnArticle(props) {
+    if (props.articleType === 'accordian') {
+        return <AccordianArticle
+                    articleTitle={props.articleTitle}
+                    articleSubtitle={props.articleSubtitle}
+                    articleBody={props.articleBody}
+                    articleAuthor={props.articleAuthor}
+                    articleDate={props.articleDate}
+                />
     } else {
-        return null;
+        return <DefaultArticle
+                    articleTitle={props.articleTitle}
+                    articleSubtitle={props.articleSubtitle}
+                    articleBody={props.articleBody}
+                    articleAuthor={props.articleAuthor}
+                    articleDate={props.articleDate}
+                />
     }
 }
 
 const Article = (props) => (
-
-
-    <div className="col-md-12">
-    <article className={`article-container ${props.articleType}`}>
-        <h2>{props.articleTitle}</h2>
-        <h3>{props.articleSubtitle}</h3>
-        <section dangerouslySetInnerHTML={
-            {__html: props.articleBody}
-        }></section>
-        <ArticleLink articleType={props.articleType} articleName={props.articleName} />
-        <footer className="article-footer">
-            <span className="article-author">Author: {props.articleAuthor}</span>
-            <span className="article-date">Date: {props.articleDate}</span>
-        </footer>
-    </article>
-    </div>
+    returnArticle(props)
 )
 
 export default Article;
---
+++ src/components/DefaultArticle.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Interactive from 'react-interactive';
+import { Switch, Route, Link } from 'react-router-dom';
+
+const Article = (props) => (
+
+
+    <div className="col-md-12">
+    <article className={`article-container ${props.articleType}`}>
+        <h2>{props.articleTitle}</h2>
+        <h3>{props.articleSubtitle}</h3>
+        <section dangerouslySetInnerHTML={
+            {__html: props.articleBody}
+        }></section>
+        <Link to={`/articles/${props.articleName}`}>
+            Click here to read
+        </Link>
+        <footer className="article-footer">
+            <span className="article-author">Author: {props.articleAuthor}</span>
+            <span className="article-date">Date: {props.articleDate}</span>
+        </footer>
+    </article>
+    </div>
+)
+
+export default Article;
